67a57bba70d64480499a4146dd0d441c6a392fc4
Jean Willian de Souza Ferreira <jeansferreira6@gmail.com>
2024-09-29 16:21:12 -0300

    Aplicado a component composition solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 function App() {
@@ -64,7 +65,13 @@ function App() {
   return (
     <>
       <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
